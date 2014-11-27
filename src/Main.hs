bd23792bf43825fef7acf04ff4f02c750cdea854
Michael Tolly <miketolly@gmail.com>
2014-11-27 14:02:14 -0600

    better resource handling w/ bracket, syntax/docs cleanup

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE PatternSynonyms #-}
-module Main where
+module Main (main) where
 
 import qualified Graphics.UI.SDL as SDL
 import qualified Graphics.UI.SDL.Image as Image
@@ -9,6 +9,7 @@ import Foreign.C
 import Control.Monad (unless, forM, forM_)
 import Control.Monad.Fix (fix)
 import Control.Concurrent (threadDelay, forkIO, killThread)
+import Control.Exception (bracket, bracket_)
 import qualified Sound.OpenAL as AL
 import qualified Sound.File.Sndfile as Snd
 import Data.Conduit
@@ -25,77 +26,102 @@ main = do
     [song] -> return song
     _      -> getProgName >>= \pn -> error $ "Usage: "++pn++" song-folder"
 
-  zero $ SDL.init $ SDL.SDL_INIT_TIMER .|. SDL.SDL_INIT_VIDEO
-  Image.imgInit [Image.InitPNG]
+  withSDL [SDL.SDL_INIT_TIMER, SDL.SDL_INIT_VIDEO]
+    $ withSDLImage [Image.InitPNG]
+    $ withWindowAndRenderer "Jelly" sheetWidth 480 SDL.SDL_WINDOW_RESIZABLE
+    $ \window rend -> do
 
-  window <- withCString "Jelly" $ \str ->
-    notNull $ SDL.createWindow
-      str -- title
+    Just info <- loadInfo song
+    Just trks <- loadTracks song
+    let fileTrks = filter (\t -> trackClass t == "JMFileTrack") trks
+    Just bts <- loadBeats song
+
+    imgs <- findNotation (head fileTrks) song
+    Right texs <- fmap sequence $ mapM (Image.imgLoadTexture rend) imgs
+    let rows = splitRows (head fileTrks) texs
+    audio <- fmap catMaybes $ mapM (`findAudio` song) fileTrks
+
+    putStrLn $ "Title: " ++ title info
+    putStrLn $ "Tracks: " ++ show (map trackTitle fileTrks)
+
+    withALContext $ do
+      hnds <- forM audio $ \a ->
+        Snd.openFile a Snd.ReadMode Snd.defaultInfo
+      srcs <- AL.genObjectNames $ length hnds * 2
+      forM_ (zip srcs $ cycle [-1, 1]) $ \(src, x) ->
+        AL.sourcePosition src AL.$= AL.Vertex3 x 0 0
+      let source = mapOutput concat $ sequenceSources $ map (`load` 1000) hnds
+          sink = void $ sequenceSinks $ do
+            (i, src) <- zip [0..] srcs
+            return $ mapInput (!! i) (const Nothing) $ supply src 5
+          isFull = fmap (all (>= 4)) $ mapM (AL.get . AL.buffersQueued) srcs
+          stretchTime = 1.2
+      tid <- forkIO $ source $$ stretch 44100 (length srcs) stretchTime 1 =$= sink
+      fix $ \loop -> isFull >>= \b -> unless b loop
+      start <- SDL.getTicks
+      AL.play srcs
+
+      let draw = do
+            now <- SDL.getTicks
+            let rowN = rowNumber bts $ (fromIntegral (now - start) / 1000) / stretchTime
+            zero $ SDL.renderClear rend
+            renderRow rend (rows !! rowN) (0, 0)
+            SDL.renderPresent rend
+      fixFrames 16 $ \loop -> do
+        draw
+        fix $ \eloop -> pollEvent >>= \case
+          Just (SDL.QuitEvent {}) -> do
+            killThread tid
+          Just (SDL.WindowEvent { SDL.windowEventEvent = SDL.SDL_WINDOWEVENT_RESIZED }) -> do
+            -- Let user adjust height, but reset width to sheetWidth
+            height <- alloca $ \pw -> alloca $ \ph -> do
+              SDL.getWindowSize window pw ph
+              peek ph
+            SDL.setWindowSize window sheetWidth height
+            eloop
+          Just _ -> eloop
+          Nothing -> loop
+
+withSDL :: [SDL.InitFlag] -> IO a -> IO a
+withSDL flags = bracket_
+  (zero $ SDL.init $ foldr (.|.) 0 flags)
+  SDL.quit
+
+withSDLImage :: [Image.InitFlag] -> IO a -> IO a
+withSDLImage flags = bracket_
+  (Image.imgInit flags)
+  Image.imgQuit
+
+withWindowAndRenderer :: String -> CInt -> CInt -> Word32
+  -> (SDL.Window -> SDL.Renderer -> IO a) -> IO a
+withWindowAndRenderer name w h flags act = bracket
+  — do
+    withCString name $ \s -> notNull $ SDL.createWindow
+      s -- title
       SDL.SDL_WINDOWPOS_UNDEFINED -- x
       SDL.SDL_WINDOWPOS_UNDEFINED -- y
-      sheetWidth -- width
-      480 -- height
-      SDL.SDL_WINDOW_RESIZABLE -- flags
-  rend <- notNull $ SDL.createRenderer window (-1) SDL.SDL_RENDERER_ACCELERATED
+      w -- width
+      h -- height
+      flags -- flags
+  — SDL.destroyWindow
+  — \window -> bracket
+    — notNull (SDL.createRenderer window (-1) 0)
+    — SDL.destroyRenderer
+    — \renderer -> act window renderer
 
-  Just info <- loadInfo song
-  Just trks <- loadTracks song
-  let fileTrks = filter (\t -> trackClass t == "JMFileTrack") trks
-  Just bts <- loadBeats song
-
-  imgs <- findNotation (head fileTrks) song
-  Right texs <- fmap sequence $ mapM (Image.imgLoadTexture rend) imgs
-  let rows = splitRows (head fileTrks) texs
-  audio <- fmap catMaybes $ mapM (`findAudio` song) fileTrks
-
-  putStrLn $ "Title: " ++ title info
-  putStrLn $ "Tracks: " ++ show (map trackTitle fileTrks)
-
-  Just dev <- AL.openDevice Nothing
-  Just ctxt <- AL.createContext dev []
-  AL.currentContext AL.$= Just ctxt
-  hnds <- forM audio $ \a ->
-    Snd.openFile a Snd.ReadMode Snd.defaultInfo
-  srcs <- AL.genObjectNames $ length hnds * 2
-  forM_ (zip srcs $ cycle [-1, 1]) $ \(src, x) ->
-    AL.sourcePosition src AL.$= AL.Vertex3 x 0 0
-  let source = mapOutput concat $ sequenceSources $ map (`load` 1000) hnds
-      sink = void $ sequenceSinks $ do
-        (i, src) <- zip [0..] srcs
-        return $ mapInput (!! i) (const Nothing) $ supply src 5
-      isFull = fmap (all (>= 4)) $ mapM (AL.get . AL.buffersQueued) srcs
-      stretchTime = 1.2
-  tid <- forkIO $ source $$ stretch 44100 (length srcs) stretchTime 1 =$= sink
-  fix $ \loop -> isFull >>= \b -> unless b loop
-  start <- SDL.getTicks
-  AL.play srcs
-
-  let draw = do
-        now <- SDL.getTicks
-        let rowN = rowNumber ((fromIntegral (now - start) / 1000) / stretchTime) bts
-        zero $ SDL.renderClear rend
-        renderRow rend (rows !! rowN) (0, 0)
-        SDL.renderPresent rend
-  fixFrames 16 $ \loop -> do
-    draw
-    fix $ \eloop -> pollEvent >>= \case
-      Just (SDL.QuitEvent {}) -> do
-        killThread tid
-        AL.currentContext AL.$= Nothing
-        AL.destroyContext ctxt
-        True <- AL.closeDevice dev
-        SDL.destroyWindow window
-        Image.imgQuit
-        SDL.quit
-      Just (SDL.WindowEvent { SDL.windowEventEvent = SDL.SDL_WINDOWEVENT_RESIZED }) -> do
-        -- Let user adjust height, but reset width to sheetWidth
-        height <- alloca $ \pw -> alloca $ \ph -> do
-          SDL.getWindowSize window pw ph
-          peek ph
-        SDL.setWindowSize window sheetWidth height
-        eloop
-      Just _ -> eloop
-      Nothing -> loop
+withALContext :: IO a -> IO a
+withALContext act = bracket
+  — AL.openDevice Nothing
+    >>= maybe (error "couldn't open audio device") return
+  — AL.closeDevice
+  — \dev -> bracket
+    — AL.createContext dev []
+      >>= maybe (error "couldn't create audio context") return
+    — AL.destroyContext
+    — \ctxt -> bracket_
+      — AL.currentContext AL.$= Just ctxt
+      — AL.currentContext AL.$= Nothing
+      — act
 
 -- | Like using "fix" to create a loop, except we start 2 iterations of the loop
 -- no faster than the given number of milliseconds apart.
@@ -110,6 +136,7 @@ fixFrames frameTime loop = go where
       threadDelay $ max 0 micro * 1000
       go
 
+-- | Extracts and throws an SDL error if the action returns a null pointer.
 notNull :: IO (Ptr a) -> IO (Ptr a)
 notNull act = do
   p <- act
@@ -117,16 +144,19 @@ notNull act = do
     then SDL.getError >>= peekCString >>= error
     else return p
 
+-- | Extracts and throws an SDL error if the action doesn't return zero.
 zero :: (Eq a, Num a) => IO a -> IO ()
 zero act = do
   n <- act
   unless (n == 0) $ SDL.getError >>= peekCString >>= error
 
+-- | Returns Just an event if there is one currently in the queue.
 pollEvent :: IO (Maybe SDL.Event)
 pollEvent = alloca $ \pevt -> SDL.pollEvent pevt >>= \case
   1 -> fmap Just $ peek pevt
   _ -> return Nothing
 
+-- | Renders a sequence of images at 1:1 aspect ratio in a vertical sequence.
 renderRow :: SDL.Renderer -> [(SDL.Texture, SDL.Rect)] -> (CInt, CInt) -> IO ()
 renderRow _    []              _      = return ()
 renderRow rend ((t, r) : rest) (x, y) = do
@@ -136,6 +166,8 @@ renderRow rend ((t, r) : rest) (x, y) = do
     zero $ SDL.renderCopy rend t p0 p1
   renderRow rend rest (x, y + SDL.rectH r)
 
+-- | Splits sheet music images into a list of rows, where each row is a
+-- sequence of texture sections to be rendered in a vertical requence.
 splitRows :: Track -> [SDL.Texture] -> [[(SDL.Texture, SDL.Rect)]]
 splitRows trk = go 0 where
   Just height = fmap fromIntegral $ scoreSystemInterval trk
@@ -152,7 +184,13 @@ splitRows trk = go 0 where
           nextRect = SDL.Rect 0 0 sheetWidth nextHeight
           in [(t, thisRect), (t', nextRect)] : go nextHeight tt
 
-rowNumber :: Double -> [Beat] -> Int
-rowNumber pos bts = let
+-- | Given the contents of beats.plist, translates a position in seconds
+-- to the sheet music row number we're on (starting from 0).
+rowNumber :: [Beat] -> Double -> Int
+rowNumber bts pos = let
   downs = map position $ filter isDownbeat bts
   in div (max 0 $ length (takeWhile (< pos) downs) - 2) 2
+
+(—) :: (a -> b) -> a -> b
+(—) = ($)
+infixl 0 —
